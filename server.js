b8084150d3226758cc702bcfeb78abb2dd981ec1
Carlos La Fuente <carlos.lafuente.dev@gmail.com>
2020-06-11 19:25:25 -0400

    Utilizando las rutas de express

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const router = express.Router();
 let app = express();
 
-app.use('/', (req, res) => {
-  res.send('Hola');
+app.use(router);
+
+router.get('/message', (req, res) => {
+  res.send('Lista de mensajes');
 });
+router.post('/message', (req, res) => {
+  res.send('Mensaje agregado correctamente');
+});
+
+// app.use('/', (req, res) => {
+//   res.send('Hola');
+// });
 
 app.listen(3000);
 console.log('La aplicacion esta escuchando en el puerto 3000');
