20dc9c7582313fbefa90aac3a95de27dc4d35eef
Carlos La Fuente <carlos.lafuente.dev@gmail.com>
2020-06-14 14:07:42 -0400

    Clase de info contextual, respuestas vacias, planas... y respuestas coherentes

--- server.js
+++ server.js
@@ -1,14 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const bodyParser = require('body-parser');
+const response = require('./network/response');
 let app = express();
 
+// El app.use del bodyParser.json() debe ser declarado antes para que funcione
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extend: false }));
 app.use(router);
 
 router.get('/message', (req, res) => {
-  res.send('Lista de mensajes');
+  console.log(req.headers);
+  res.header({
+    'custom-header': 'Header personalizado',
+  });
+  // res.send('Lista de mensajes');
+  response.success(req, res, 'Lista de mensajes');
 });
 router.post('/message', (req, res) => {
-  res.send('Mensaje agregado correctamente');
+  console.log(req.query);
+  console.log(req.body);
+  // res.status(201).send({
+  //   error: '',
+  //   message: 'Creado correctamente',
+  //   value: req.body,
+  // });
+  if (req.query.error == 'ok') {
+    response.error(req, res, 'Error simulado', 400);
+  } else {
+    response.success(req, res, 'Creado correctamente', 201);
+  }
 });
 
 // app.use('/', (req, res) => {
