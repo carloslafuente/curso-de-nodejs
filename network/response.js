da748c4bf11b429398c270d29a45a55dcdaa8124
Carlos La Fuente <carlos.lafuente.dev@gmail.com>
2020-06-16 11:54:43 -0400

    refactorizando el codigo

--- network/response.js
+++ network/response.js
@@ -1,14 +1,29 @@
+const statusMessages = {
+	200: 'Done',
+	201: 'Created',
+	400: 'Invalid format',
+	500: 'Internal error',
+};
+
 exports.success = (req, res, message, status) => {
-  res.status(status || 200).send({
-    error: '',
-    body: message,
-  });
+	let statusCode = status;
+	let statusMessage = message;
+	if (!status) {
+		status = 200;
+	}
+	if (!message) {
+		statusMessage = statusMessages[status];
+	}
+	res.status(statusCode).send({
+		error: '',
+		body: statusMessage,
+	});
 };
 
 exports.error = (req, res, message, status, details) => {
-  console.error(details);
-  res.status(status || 500).send({
-    error: message,
-    body: '',
-  });
+	console.error(details);
+	res.status(status || 500).send({
+		error: message,
+		body: '',
+	});
 };
